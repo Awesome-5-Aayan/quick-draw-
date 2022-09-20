3f0b7e4d7adaa4b307b0dc2de0a19ea48a2dcfb2
Awesome-5-Aayan <81710455+Awesome-5-Aayan@users.noreply.github.com>
2022-09-20 17:55:25 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,43 @@ var timer_check = "";
 var drawn_sketch = "";
 var answer_holder="";
 var score = 0;
+function updateCanvas(){
+background("white");
+random_number = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+console.log(quick_draw_data_set[random_number]);
+var sketch = quick_draw_data_set[random_number];
+document.getElementById('sketch_name').innerHTML='Sketch to be Drawn: '+sketch;
+}
+function setup(){
+    canvas=createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+function draw(){
+    strokeWeight(13);
+    stoke(0);
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+
+    }
+    if (drawn_sketch==sketch) {
+        answer_holder="set";
+        score++;
+        document.getElementById('score').innerHTML="Score: "+ score;
+    }
+    check_sketch()
+}
+function check_sketch(){
+    timer_counter++;
+    document.getElementById('timer').innerHTML="Timer: "+timer_counter;
+    if (timer_counter>400) {
+        timer_counter=0;
+        timer_check="completed";
+
+    }
+ if (timer_check=="completed" ||answer_holder=="set") {
+    timer_check="";
+    answer_holder="";
+    updateCanvas();
+ }
+}
